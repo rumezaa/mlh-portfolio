7470dbf032d667ebde16f86ea5aeb2190b608066
rumezaa <rumeza06@gmail.com>
2025-06-23 13:08:57 -0600

    Create places I've visited feat

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+def transform_location(location):
+    return location.replace(" ", "+")
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    # for jinja template, update when that pr is merged
+    google_maps_api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    locations = ["Empire State Building, NY"]
+    transformed_locations = [transform_location(location) for location in locations]
+
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), locations=",".join(transformed_locations), google_maps_api_key=google_maps_api_key)
+
+
