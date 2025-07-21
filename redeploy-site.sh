09c54bad7c3bc7493ff0f61c4484443d0547d778
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-07-21 00:50:11 -0400

    systemd service for deploying

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions (stops old Flask server)
-tmux kill-server
-
-# Navigate to your project directory
-cd ~/mlh-portfolio || {
-    echo "Directory ~/mlh-portfolio not found!"
-    exit 1
-}
-
 # Pull latest changes from GitHub main branch
 git fetch && git reset origin/main --hard
 
@@ -18,5 +9,6 @@ source python3-virtualenv/bin/activate
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Start a new detached tmux session that launches the Flask server
-tmux new-session -d -s flask-app "bash -c 'cd ~/mlh-portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'"
+# restart systemd service and show status
+sudo systemctl restart myportfolio
+sudo systemctl status myportfolio
