eef80fe38a38db1e86c938295d62158936070e03
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-08-03 16:37:41 -0400

    docker compose with mariadb service and server

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,20 @@ from peewee import *
 from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
+print("→ ENV TESTING is:", repr(os.getenv("TESTING")))
 app = Flask(__name__)
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306,
-)
+if os.getenv("TESTING") == "true":
+    print("RUNNING IN TEST MODE")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared&uri=true')
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
 print(mydb)
 
 class TimelinePost(Model):
